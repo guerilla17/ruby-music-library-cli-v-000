34d764ef496fa1452d20e15795ce01e1e6f15a21
Gigi M <mmgamao1@gmail.com>
2019-07-11 03:16:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,12 +36,8 @@ class Song
   end
 
   def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
-binding.pry  
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    self.new(song_name, artist, genre)
+      song = new_from_filename(filename)
+      song.save  
      
   end
 
