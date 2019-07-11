8b6b3d181b4e3597e6f7fc043ac087f9312cbaae
Gigi M <mmgamao1@gmail.com>
2019-07-11 02:53:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     genre = Genre.find_or_create_by_name(genre_name)
     self.new(song_name, artist, genre)
   end
-binding.pry
+binding.pry 
   def self.create_from_filename(filename)
     parts = filename.split(" - ")
     artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
